f72691736f53c0c8d9d3659466866a05bd9ddf99
Fredy <flangat282@gmail.com>
2025-06-23 11:57:05 +0300

    V 1.0.0

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.jsCheckButton').addEventListener('click', () => {
+    let  ageInputElement = document.querySelector('.jsAgeInput');
+
+    let age = ageInputElement.value;
+    console.log(age);
+
+    if (age >= 18) {
+        document.querySelector('.jsShowMessage').innerHTML = 'Is of age and fit to drive'
+    } else {
+        document.querySelector('.jsShowMessage').innerHTML = 
+        'Is not of age and cannot drive';
+    }
+})
